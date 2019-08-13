1890463abc80db1086f52f1314f00e66443449dc
Stefan Hyltoft <stefanhyltoft@gmail.com>
2019-08-13 11:38:37 +0200

    Unable to get in memory db working

--- tests/integration/todo.routes.spec.js
+++ tests/integration/todo.routes.spec.js
@@ -1,20 +1,37 @@
-// const chai = require("chai");
-// const chaiHttp = require("chai-http");
-// chai.use(chaiHttp);
-// const expect = chai.expect;
-// const request = chai.request;
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+chai.use(chaiHttp);
+const expect = chai.expect;
+const request = chai.request;
+const Mongoose = require("mongoose").Mongoose;
+const Mockgoose = require("mockgoose-fix").Mockgoose;
+const mongoose = new Mongoose();
+const mockgoose = new Mockgoose(mongoose);
 
-// const newTodo = require("../mock-data/new-todo.json");
-// const app = require("../../app");
+mongoose.Promise = global.Promise;
+mockgoose.helper.setDbVersion("3.4.3");
 
-// describe("TodoRoutes", () => {
-//   it("should create a a new todo", done => {
-//     request(app)
-//       .post("/todos")
-//       .send(newTodo)
-//       .end((err, res) => {
-//         expect(res.status).to.be.equal(201);
-//         done();
-//       });
-//   });
-// });
+const mockMongoDBURL = "mongodb://localhost:32768/mockCustomerDB";
+
+before(done => {
+  mockgoose.prepareStorage().then(() => {
+    mongoose.connect(mockMongoDBURL, { useNewUrlParser: true }, err => {
+      done(err);
+    });
+  });
+});
+
+const newTodo = require("../mock-data/new-todo.json");
+const app = require("../../app");
+
+describe("TodoRoutes", () => {
+  it("should create a a new todo", done => {
+    request(app)
+      .post("/todos")
+      .send(newTodo)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(201);
+        done();
+      });
+  });
+});
